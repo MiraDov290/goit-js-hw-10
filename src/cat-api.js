ea5f24e9603d6ce393be9e112e7a3a08e1a6cc4b
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-07-04 11:20:46 +0100

    changers

---
+++ src/cat-api.js
@@ -0,0 +1,22 @@
+const url = 'https://api.thecatapi.com/v1/images/search';
+const api_key = "live_FcpssCX9H7WABYUGsNiwRLRumKB20H9RdEt6hRiMDWkCinGFjGECDSZFCei21kb6";
+
+export function fetchBreeds() {
+    return fetch(`${url}/breeds?api_key=${api_key}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });       
+};
+
+export function fetchCatByBreed(breedId) {
+    return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });  
+};
