635d74eaf2144ebeb75934854f307af10eec47ee
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-07-05 10:41:10 +0100

    chang

--- src/cat-api.js
+++ src/cat-api.js
@@ -22,12 +22,3 @@ export function fetchCatByBreed(breedId) {
         });
 };
 
-// ﻿export function fetchCountries(name) {
-//     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-//         .then(response => {
-//             if (response.ok) {
-//                 return response.json();
-//             }
-//             throw new Error(response.statusText);
-//         });
-// };
